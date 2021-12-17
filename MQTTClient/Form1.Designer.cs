4222e16ec32b408b0bacb3dee2b6240bfea467e9
黎树平 <1326702940@qq.com>
2021-12-17 18:08:20 +0800

    添加项目文件。

---
+++ MQTTClient/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MQTTClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtTopic = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPayload = new System.Windows.Forms.TextBox();
+            this.btnPublish = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnSubscribe = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(54, 57);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtTopic
+            // 
+            this.txtTopic.Location = new System.Drawing.Point(54, 301);
+            this.txtTopic.Name = "txtTopic";
+            this.txtTopic.Size = new System.Drawing.Size(166, 21);
+            this.txtTopic.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "主题";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "内容";
+            // 
+            // txtPayload
+            // 
+            this.txtPayload.Location = new System.Drawing.Point(54, 382);
+            this.txtPayload.Name = "txtPayload";
+            this.txtPayload.Size = new System.Drawing.Size(166, 21);
+            this.txtPayload.TabIndex = 4;
+            // 
+            // btnPublish
+            // 
+            this.btnPublish.Location = new System.Drawing.Point(145, 415);
+            this.btnPublish.Name = "btnPublish";
+            this.btnPublish.Size = new System.Drawing.Size(75, 23);
+            this.btnPublish.TabIndex = 5;
+            this.btnPublish.Text = "发布";
+            this.btnPublish.UseVisualStyleBackColor = true;
+            this.btnPublish.Click += new System.EventHandler(this.btnPublic_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(54, 108);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessage.Size = new System.Drawing.Size(166, 147);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(145, 57);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 7;
+            this.btnDisconnect.Text = "断开";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnSubscribe
+            // 
+            this.btnSubscribe.Location = new System.Drawing.Point(54, 415);
+            this.btnSubscribe.Name = "btnSubscribe";
+            this.btnSubscribe.Size = new System.Drawing.Size(75, 23);
+            this.btnSubscribe.TabIndex = 8;
+            this.btnSubscribe.Text = "订阅";
+            this.btnSubscribe.UseVisualStyleBackColor = true;
+            this.btnSubscribe.Click += new System.EventHandler(this.txtSubscribe_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(54, 25);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(166, 21);
+            this.txtIP.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 450);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnSubscribe);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnPublish);
+            this.Controls.Add(this.txtPayload);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTopic);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtTopic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPayload;
+        private System.Windows.Forms.Button btnPublish;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnSubscribe;
+        private System.Windows.Forms.TextBox txtIP;
+    }
+}
+
---
+++ MQTTClient/Form1.cs
@@ -0,0 +1,369 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MQTTClient
+{
+    public partial class Form1 : Form
+    {
+        public MqttClient mqttClient = null;
+
+        public List<MqttClient> mqttClients = new List<MqttClient>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ClientStart();
+            //return;
+
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 50000; i++)
+            //    {
+            //        //Thread.Sleep(500);
+            //        ClientStart();
+            //    }
+            //});
+
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 2000; i++)
+            //    {
+            //        //Thread.Sleep(10);
+            //        ClientStart();
+            //    }
+            //});
+
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 2000; i++)
+            //    {
+            //        //Thread.Sleep(10);
+            //        ClientStart();
+            //    }
+            //});
+
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 2000; i++)
+            //    {
+            //        //Thread.Sleep(10);
+            //        ClientStart();
+            //    }
+            //});
+
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 2000; i++)
+            //    {
+            //        //Thread.Sleep(10);
+            //        ClientStart();
+            //    }
+            //});
+
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 2000; i++)
+            //    {
+            //        //Thread.Sleep(10);
+            //        ClientStart();
+            //    }
+            //});
+
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 5000; i++)
+            //    {
+            //        //Thread.Sleep(500);
+            //        ClientStart();
+            //    }
+            //});
+        }
+
+        public async void ClientStart()
+        {
+            try
+            {
+                var tcpServer = string.IsNullOrWhiteSpace(this.txtIP.Text?.Trim()) ? "127.0.0.1" : this.txtIP.Text.Trim();
+                var tcpPort = 1883;
+                var mqttUser = "mqtt-test";
+                var mqttPassword = "mqtt-test";
+                var clientId = "lsp" + Guid.NewGuid();
+
+                mqttUser = "test-host:admin-test";
+                mqttPassword = "test";
+
+                var mqttFactory = new MqttFactory();
+
+                var options = new MqttClientOptions
+                {
+                    ClientId = clientId,
+                    ProtocolVersion = MQTTnet.Formatter.MqttProtocolVersion.V311,
+                    ChannelOptions = new MqttClientTcpOptions
+                    {
+                        Server = tcpServer,
+                        Port = tcpPort
+                    },
+                    WillDelayInterval = 10,
+                    WillMessage = new MqttApplicationMessage
+                    {
+                        Topic = $"LastWill/{clientId}",
+                        Payload = Encoding.UTF8.GetBytes("I Lost the Connection!"),
+                        QualityOfServiceLevel = MQTTnet.Protocol.MqttQualityOfServiceLevel.ExactlyOnce
+                    }
+                };
+
+                if (options.ChannelOptions == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                if (!string.IsNullOrWhiteSpace(mqttUser))
+                {
+                    options.Credentials = new MqttClientCredentials
+                    {
+                        Username = mqttUser,
+                        Password = Encoding.UTF8.GetBytes(mqttPassword)
+                    };
+                }
+
+                options.CleanSession = true;
+                options.KeepAlivePeriod = TimeSpan.FromSeconds(10);
+
+                mqttClient = mqttFactory.CreateMqttClient() as MqttClient;
+                //mqttClient = mqttFactory.CreateMqttClient() as MqttClient;
+                //mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnMqttClientConnected);
+                mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(new Action<MqttClientConnectedEventArgs>(e =>
+                {
+                    LogManager.WriteLogEx(LOGLEVEL.INFO, "客户端已连接");
+                    this.txtMessage.BeginInvoke(new Action(() =>
+                    {
+                        this.txtMessage.Text += "客户端已连接" + Environment.NewLine;
+                    }));
+                    //mqttClient.SubscribeAsync("lsp");
+                    //LogManager.WriteLogEx(LOGLEVEL.INFO, "订阅lsp");
+                }));
+
+                mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(new Action<MqttClientDisconnectedEventArgs>(e =>
+                {
+                    LogManager.WriteLogEx(LOGLEVEL.INFO, "客户端已断开连接");
+                    this.txtMessage.BeginInvoke(new Action(() =>
+                    {
+                        this.txtMessage.Text += "客户端已断开连接" + Environment.NewLine;
+                    }));
+
+                    //mqttClient.ConnectAsync(options, CancellationToken.None);
+                }));
+
+                mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(new Action<MqttApplicationMessageReceivedEventArgs>(e =>
+                {
+                    string text = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    string topic = e.ApplicationMessage.Topic;
+                    string qos = e.ApplicationMessage.QualityOfServiceLevel.ToString();
+                    string retained = e.ApplicationMessage.Retain.ToString();
+
+                    this.txtMessage.BeginInvoke(new Action(() =>
+                    {
+                        this.txtMessage.Text += $"客户端接收消息 >>Topic：{topic}; Qos：{qos}; Retained：{retained}" + Environment.NewLine;
+                        this.txtMessage.Text += $"客户端接收消息 >>Msg：{text}" + Environment.NewLine;
+                    }));
+
+                    LogManager.WriteLogEx(LOGLEVEL.INFO, $"客户端接收消息 >>Topic：{topic}; Qos：{qos}; Retained：{retained}");
+                    LogManager.WriteLogEx(LOGLEVEL.INFO, $"客户端接收消息 >>Msg：{text}");
+                }));
+
+                //mqttClients.Add(mqttClient);
+                await mqttClient.ConnectAsync(options);
+
+                LogManager.WriteLogEx(LOGLEVEL.INFO, $"客户端{options.ClientId}尝试连接...");
+                this.txtMessage.BeginInvoke(new Action(() =>
+                {
+                    this.txtMessage.Text += $"客户端{options.ClientId}尝试连接..." + Environment.NewLine;
+                }));
+            }
+            catch (Exception ex)
+            {
+                LogManager.WriteLogEx(LOGLEVEL.ERROR, $"客户端尝试连接出错：" + ex.Message);
+
+                //await mqttClients[0].ReconnectAsync();
+
+                this.txtMessage.BeginInvoke(new Action(() =>
+                {
+                    this.txtMessage.Text += $"客户端尝试连接出错：" + ex.Message + Environment.NewLine;
+                }));
+            }
+        }
+
+        private Task OnMqttClientConnected(MqttClientConnectedEventArgs obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            ClientStop();
+        }
+
+        public async void ClientStop()
+        {
+            try
+            {
+                if (mqttClient == null)
+                {
+                    return;
+                }
+
+                await mqttClient.DisconnectAsync();
+                mqttClient = null;
+            }
+            catch (Exception ex)
+            {
+                this.txtMessage.BeginInvoke(new Action(() =>
+                {
+                    this.txtMessage.Text += $"客户端尝试断开连接出错：" + ex.Message + Environment.NewLine;
+                }));
+            }
+        }
+
+        /// <summary>
+        /// 订阅
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtSubscribe_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (mqttClient == null || !mqttClient.IsConnected)
+                {
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(this.txtTopic.Text))
+                {
+                    this.txtMessage.BeginInvoke(new Action(() =>
+                    {
+                        this.txtMessage.Text += $"客户端订阅主题不能为空" + Environment.NewLine;
+                    }));
+
+                    return;
+                }
+
+                ClientSubscribeTopic(this.txtTopic.Text);
+            }
+            catch (Exception ex)
+            {
+                this.txtMessage.BeginInvoke(new Action(() =>
+                {
+                    this.txtMessage.Text += $"客户端订阅出错：" + ex.Message + Environment.NewLine;
+                }));
+            }
+        }
+
+        public async void ClientSubscribeTopic(string topic)
+        {
+            await mqttClient.SubscribeAsync(topic);
+
+            this.txtMessage.BeginInvoke(new Action(() =>
+            {
+                this.txtMessage.Text += $"客户端{mqttClient.Options.CleanSession}订阅{topic}成功!" + Environment.NewLine;
+            }));
+        }
+
+        /// <summary>
+        /// 发布
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPublic_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (mqttClient == null || !mqttClient.IsConnected)
+                {
+                    return;
+                }
+
+                var topic = this.txtTopic.Text;
+                var payload = this.txtPayload.Text;
+
+                if (string.IsNullOrWhiteSpace(topic))
+                {
+                    this.txtMessage.BeginInvoke(new Action(() =>
+                    {
+                        this.txtMessage.Text += $"客户端发布主题不能为空" + Environment.NewLine;
+                    }));
+
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(payload))
+                {
+                    this.txtMessage.BeginInvoke(new Action(() =>
+                    {
+                        this.txtMessage.Text += $"客户端发布内容不能为空" + Environment.NewLine;
+                    }));
+
+                    return;
+                }
+
+                ClientPublishTopic(topic, payload);
+            }
+            catch (Exception ex)
+            {
+                this.txtMessage.BeginInvoke(new Action(() =>
+                {
+                    this.txtMessage.Text += $"客户端发布主题出错：" + ex.Message + Environment.NewLine;
+                }));
+
+                ClientStop();
+            }
+
+        }
+
+        public async void ClientPublishTopic(string topic, string payload)
+        {
+            try
+            {
+                var message = new MqttApplicationMessage
+                {
+                    Topic = topic,
+                    Payload = Encoding.UTF8.GetBytes(payload),
+                    QualityOfServiceLevel = MQTTnet.Protocol.MqttQualityOfServiceLevel.AtMostOnce,
+                    Retain = true
+                };
+
+                await mqttClient.PublishAsync(message, CancellationToken.None);
+
+                this.txtMessage.BeginInvoke(new Action(() =>
+                {
+                    this.txtMessage.Text += $"客户端{mqttClient.Options.ClientId}发布主题{topic}成功" + Environment.NewLine;
+                }));
+            }
+            catch (Exception ex)
+            {
+                this.txtMessage.BeginInvoke(new Action(() =>
+                {
+                    this.txtMessage.Text += $"客户端{mqttClient.Options.ClientId}发布主题{topic}出错：" + ex.Message + Environment.NewLine;
+                }));
+            }
+        }
+    }
+}
